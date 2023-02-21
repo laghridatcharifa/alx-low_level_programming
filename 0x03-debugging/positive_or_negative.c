b296e9c887e7c4abe84e19585bdc8b770007175b
laghridatcharifa <laghridatcharifa@gmail.com>
2023-02-21 15:38:02 -0800

    Commit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Program that print a line using printf
+ *
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative(int n)
+{
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+
+}
