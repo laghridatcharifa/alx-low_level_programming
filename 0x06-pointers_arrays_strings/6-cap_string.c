189d08d209cca0c10dcd8d909bc4932d134777ae
laghridatcharifa <laghridatcharifa@gmail.com>
2023-03-01 12:08:25 -0800

    task done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,14 +12,14 @@ while (str[i])
 {
 while (!(str[i] >= 'a' && str[i] <= 'z'))
 i++;
-if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n')
-if (str[i - 1] == ',' || str[i - 1] == ';')
-if (str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?')
-if (str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')')
-if (str[i - 1] == '{' || str[i - 1] == '}' || i == 0)
+if (str[i - 1] == ' ' ||
+str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' ||
+str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' ||
+str[i - 1] == '?' || str[i - 1] == '"' ||
+str[i - 1] == '(' || str[i - 1] == ')' ||
+str[i - 1] == '{' || str[i - 1] == '}' || i == 0)
 str[i] -= 32;
 i++;
 }
 return (str);
 }
-
